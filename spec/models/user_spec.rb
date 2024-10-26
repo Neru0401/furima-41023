3ee470c59319e9c872871951106ae16e127e8829
Neru0401 <raranoneru@gmail.com>
2024-10-26 17:21:38 +0900

    正常系テストの追加とFactorybotの修正

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,6 +6,11 @@ RSpec.describe User, type: :model do
   end
 
   describe 'ユーザー新規登録' do
+    context '新規登録できる場合' do
+      it "全ての項目に正しく入力できている" do
+        expect(@user).to be_valid
+      end
+    end
     context '新規登録できない場合' do
       it 'nicknameが空では登録できない' do
         @user.nickname = ''
