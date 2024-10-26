7f409e532079b207d7426a868c5f4eebcf62fd8a
Neru0401 <raranoneru@gmail.com>
2024-10-27 01:48:50 +0900

    モデルのアソシエーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,12 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one :purchase
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :deliveryfee
+  belongs_to :prefecture
+  belongs_to :shipdate
+
 end
