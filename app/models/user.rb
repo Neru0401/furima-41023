0c7bfee76d00cb04fd03ce0a0076eb3efa7897ce
Neru0401 <raranoneru@gmail.com>
2024-10-26 16:36:10 +0900

    Rubocopの実行

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   validates :nickname, presence: true
   validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角(漢字・ひらがな・カタカナ)文字が必須' }
-  validates :first_name, presence: true,format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角(漢字・ひらがな・カタカナ)文字が必須' }
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角(漢字・ひらがな・カタカナ)文字が必須' }
   validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー]+\z/, message: '全角（カタカナ）文字が必須' }
   validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー]+\z/, message: '全角（カタカナ）文字が必須' }
   validates :birth, presence: true
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -36,41 +36,41 @@ RSpec.describe User, type: :model do
         expect(another_user.errors.full_messages).to include('Email has already been taken')
       end
       it 'passwordが5文字以下だと登録できない' do
-        @user.password = "12abc"
+        @user.password = '12abc'
         @user.password_confirmation = '12abc'
         @user.valid?
-        expect(@user.errors.full_messages).to include ("Password is too short (minimum is 6 characters)")
+        expect(@user.errors.full_messages).to include('Password is too short (minimum is 6 characters)')
       end
       it 'passwordは数字のみだと登録できない' do
-        @user.password = "000000"
+        @user.password = '000000'
         @user.valid?
         expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
       end
       it 'passwordは英字のみだと登録できない' do
-        @user.password = "aaaaaa"
+        @user.password = 'aaaaaa'
         @user.valid?
         expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
       end
       it 'last_nameが空では登録できない' do
         @user.last_name = ''
         @user.valid?
-        expect(@user.errors.full_messages).to include ("Last name can't be blank")
+        expect(@user.errors.full_messages).to include("Last name can't be blank")
       end
       it 'first_nameが空では登録できない' do
         @user.last_name_kana = ''
         @user.valid?
-        expect(@user.errors.full_messages).to include ("Last name kana can't be blank")
+        expect(@user.errors.full_messages).to include("Last name kana can't be blank")
       end
       it 'last_nameは全角(漢字・ひらがな・カタカナ)文字が必須' do
         @user.last_name = 'aaaa'
         @user.valid?
-        expect(@user.errors.full_messages).to include("Last name 全角(漢字・ひらがな・カタカナ)文字が必須")
+        expect(@user.errors.full_messages).to include('Last name 全角(漢字・ひらがな・カタカナ)文字が必須')
       end
       it 'first_nameは全角(漢字・ひらがな・カタカナ)文字が必須' do
         @user.first_name = 'aaaa'
         @user.valid?
-        expect(@user.errors.full_messages).to include("First name 全角(漢字・ひらがな・カタカナ)文字が必須")
-      end 
+        expect(@user.errors.full_messages).to include('First name 全角(漢字・ひらがな・カタカナ)文字が必須')
+      end
       it 'last_name_kanaが空では登録できない' do
         @user.last_name_kana = ''
         @user.valid?
@@ -84,19 +84,18 @@ RSpec.describe User, type: :model do
       it 'last_name_kanaは全角（カタカナ）文字が必須' do
         @user.last_name_kana = 'aaaa'
         @user.valid?
-        expect(@user.errors.full_messages).to include("Last name kana 全角（カタカナ）文字が必須")
+        expect(@user.errors.full_messages).to include('Last name kana 全角（カタカナ）文字が必須')
       end
       it 'first_name_kanaは全角（カタカナ）文字が必須' do
         @user.first_name_kana = 'aaaa'
         @user.valid?
-        expect(@user.errors.full_messages).to include("First name kana 全角（カタカナ）文字が必須")
+        expect(@user.errors.full_messages).to include('First name kana 全角（カタカナ）文字が必須')
       end
       it 'birthが空では登録できない' do
         @user.birth = ''
         @user.valid?
-        expect(@user.errors.full_messages).to include ("Birth can't be blank")
+        expect(@user.errors.full_messages).to include("Birth can't be blank")
       end
     end
   end
-
 end
