53294f59f1d30f5bc2d75ef47f32775240423dfa
Neru0401 <raranoneru@gmail.com>
2024-10-27 12:20:42 +0900

    numericalityの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,4 +10,12 @@ class Item < ApplicationRecord
   belongs_to :prefecture
   belongs_to :shipdate
 
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :category_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
+  validates :condition_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
+  validates :delivery_fee_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
+  validates :prefecture_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
+  validates :ship_date_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
+  validates :price, presence: true
 end
