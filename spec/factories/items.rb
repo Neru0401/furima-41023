e822906bda833c71cded0ad2faef4f959468acdf
Neru0401 <raranoneru@gmail.com>
2024-10-27 19:39:04 +0900

    FactoryBotの編集

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,18 @@
 FactoryBot.define do
   factory :item do
+    title {'タイトル'}
+    description {'商品の説明'}
+    category_id { 2 }
+    condition_id { 2 }
+    delivery_fee_id { 2 }
+    prefecture_id { 2 }
+    ship_date_id { 2 }
+    price { 1000 }
+    association :user
     
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
+
   end
 end
