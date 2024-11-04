a29d20b3641245b30214662da03ac1784351f978
Neru0401 <raranoneru@gmail.com>
2024-11-04 17:33:26 +0900

    商品情報編集ページの制限

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_item, only: [:show, :edit, :update]
+  before_action :check_owner, only: [:edit, :update]
 
   def index
     @items = Item.includes(:user).order('created_at DESC')
@@ -19,15 +21,12 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path(@item)
     else
@@ -36,6 +35,14 @@ class ItemsController < ApplicationController
   end
   
   private
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+  def check_owner
+    redirect_to root_path unless @item.user == current_user
+  end
 
   def item_params
     params.require(:item).permit(
