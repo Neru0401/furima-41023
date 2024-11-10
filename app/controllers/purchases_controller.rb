b2c3b0d2a09a1593352098749c2cc1c4ee334e4b
Neru0401 <raranoneru@gmail.com>
2024-11-10 18:13:06 +0900

    コントローラーへの記述

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,2 +1,33 @@
 class PurchasesController < ApplicationController
+  before_action :set_item, only: [:index, :create]
+  def index
+    @purchase_shipping = PurchaseShipping.new
+  end
+  def create
+    @purchase_shipping = PurchaseShipping.new(purchase_params)
+    if @purchase_shipping.valid?
+      @purchase_shipping.save
+      redirect_to root_path
+    else
+      render :index, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
+  def purchase_params
+    params.require(:purchase).permit(
+      :postal_code,
+      :prefecture_id,
+      :city,
+      :address,
+      :building,
+      :telephone_number
+      ).merge(item_id: @item.id, user_id: current_user.id)
+  end
+
 end
